e1529f5e0119f1d57785aeb74a38d2de2ea960c8
HJ.Park <hyunjun19@gmail.com>
2018-02-07 23:16:49 +0900

    URL에 http 프로토콜이 없는 경우 자동으로 붙여주는 appendHttpProtocal 메서드 추가

--- test/grep-http-test.js
+++ test/grep-http-test.js
@@ -7,6 +7,7 @@ const grepHttpOpenGraph = require('../lib/grep-http-open-graph');
 
 const naverBlogUrl = 'http://blog.naver.com/PostView.nhn?blogId=natur_herz&logNo=221021152411';
 const gajagoDealUrl = 'https://www.thegajago.com/deals/22008';
+const noProtocalUrl = 'www.thegajago.com/deals/19';
 
 const customFields = [
     {
@@ -49,11 +50,23 @@ describe('HTTP TEST', () => {
                 // console.log('\n\n### GajagoDeal ###\n', openGraphObj);
 
                 assert.equal(openGraphObj.ogTitle, '[충남] 태안투어패스 (구매 후 익일 사용가능) 4,000원 - 대한민국 어디든, 가자고!', '타이틀이 틀렸어...');
-                assert.equal(openGraphObj.social, '<a data-href="https://www.facebook.com/thegajago/" title="facebook"><i class="icon-facebook"></i></a><a data-href="http://blog.naver.com/start1q" title="naver blog"><i class="icon-blog"></i></a><a data-href="https://www.youtube.com/channel/UC5GHZ6AL_yXpxnPcATCER7w" title="youtube"><i class="icon-youtube-line"></i></a><a data-href="https://www.instagram.com/gajago_insta/" title="instagram"><i class="icon-instagram-line"></i></a>', '소셜 HTML을 못가져왔네...');
+                assert.equal(openGraphObj.social, '<a data-href="https://www.facebook.com/thegajago/" title="facebook" data-gtm-event="home__click_hometop" data-gtm-options="hometop_name:&#xD398;&#xC774;&#xC2A4;&#xBD81;"><i class="icon-facebook"></i></a><a data-href="http://blog.naver.com/start1q" title="naver blog" data-gtm-event="home__click_hometop" data-gtm-options="hometop_name:&#xBE14;&#xB7EC;&#xADF8;"><i class="icon-blog"></i></a><a data-href="https://www.youtube.com/channel/UC5GHZ6AL_yXpxnPcATCER7w" title="youtube" data-gtm-event="home__click_hometop" data-gtm-options="hometop_name:&#xC720;&#xD29C;&#xBE0C;"><i class="icon-youtube-line"></i></a><a data-href="https://www.instagram.com/gajago_insta/" title="instagram" data-gtm-event="home__click_hometop" data-gtm-options="hometop_name:&#xC778;&#xC2A4;&#xD0C0;&#xADF8;&#xB7A8;"><i class="icon-instagram-line"></i></a>', '소셜 HTML을 못가져왔네...');
+
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    it('트로토콜이 없는 URL 테스트', (done) => {
+        grepHttpOpenGraph(noProtocalUrl, customFields)
+            .then((openGraphObj) => {
+                // console.log('\n\n### GajagoDeal ###\n', openGraphObj);
+
+                assert.equal(openGraphObj.ogTitle, '[전주] 말순이 : 한옥마을 한복대여권 3,000원 - 대한민국 어디든, 가자고!', '타이틀이 틀렸어...');
+                assert.equal(openGraphObj.ogUrl, 'http://www.thegajago.com/deals/19', 'ogUrl을 못가져왔네...');
 
                 done();
             })
             .catch((err) => done(err));
     });
 });
-    
